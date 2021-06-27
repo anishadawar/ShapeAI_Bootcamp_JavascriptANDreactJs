f677a73c3cae3ab550dba938cbbf51af62cddaa7
anishadawar <86565733+anishadawar@users.noreply.github.com>
2021-06-27 21:55:44 +0530

    Add files via upload

---
+++ Info.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Info() {
+  return (
+    <div className="note">
+      <h1>JavaScript and React js </h1>
+      <p>
+        Learn Front end development with Basics of Javascript and React js with
+        hands on project,no prequistics required!
+      </p>
+    </div>
+  );
+}
+export default Info;
